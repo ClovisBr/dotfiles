1ce07fc4303c3b8cd2121a02830b3ada8a478785
Clovis BROGGIO <clovis.broggio@outlook.fr>
2025-11-09 15:51:13 +0100

    configuration de nvim

--- scripts/.config/scripts/pacman-update.sh
+++ scripts/.config/scripts/pacman-update.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+LOCKFILE=/tmp/pacman-update.lock
+if [ -e "$LOCKFILE" ]; then
+    echo "Another update is running, exiting."
+    exit 1
+fi
+touch "$LOCKFILE"
 /usr/bin/pacman -Syu --noconfirm >> /var/log/pacman-auto-update.log 2>&1
+rm "$LOCKFILE"
+
